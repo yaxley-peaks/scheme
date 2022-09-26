8ed8eddc65d3b3c50ff1fe50ff25f68f8a959a4b
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-09-26 17:31:58 +0530

    showval

--- app/Etor.hs
+++ app/Etor.hs
@@ -1,1 +1,14 @@
 module Etor where
+
+import Data (LispVal (..))
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number content) = show content
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ "." ++ showVal tail
+
+unwordsList = unwords . map showVal
