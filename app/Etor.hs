026dd89a57b60b9bc2fa5635964415d060f1eb85
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-09-26 19:48:23 +0530

    equal

--- app/Etor.hs
+++ app/Etor.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Etor where
 
-import Control.Monad.Error (MonadError (throwError))
+import Control.Monad.Error (MonadError (catchError, throwError))
 import Data (LispVal (..))
 import Error (LispError (BadSpecialForm, NotFunction, NumArgs, TypeMismatch), ThrowsError)
 
@@ -54,7 +55,8 @@ primitives =
     ("cdr", cdr),
     ("cons", cons),
     ("eqv?", eqv),
-    ("eq?", eqv)
+    ("eq?", eqv),
+    ("equal?", equal)
   ]
 
 boolBinOp :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
@@ -137,3 +139,24 @@ eqv [List arg1, List arg2] =
       Right (Bool val) -> val
 eqv [_, _] = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+  do
+    unpacked1 <- unpacker arg1
+    unpacked2 <- unpacker arg2
+    return $ unpacked1 == unpacked2
+    `catchError` const (return False)
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+  primitiveEquals <-
+    or
+      <$> mapM
+        (unpackEquals arg1 arg2)
+        [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+  eqvEquals <- eqv [arg1, arg2]
+  return $ Bool (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
