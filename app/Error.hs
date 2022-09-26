c6357144053f4d5c028cb9e12b23f308da04e825
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-09-26 18:35:08 +0530

    Error types

--- app/Error.hs
+++ app/Error.hs
@@ -1,1 +1,17 @@
+{-# OPTIONS_GHC -Wno-deprecations #-}
+
 module Error where
+
+import Control.Monad.Error
+import Data (LispVal)
+import Text.Parsec (ParseError)
+
+data LispError
+  = NumArgs Integer [LispVal]
+  | TypeMismatch String LispVal
+  | Parser ParseError
+  | BadSpecialForm String LispVal
+  | NotFunction String String
+  | UnboundVar String String
+  | Default String
+
