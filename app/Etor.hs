9134cf5506ae83fd03af176761a90e97e0b79604
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-09-26 19:19:08 +0530

    more primitives

--- app/Etor.hs
+++ app/Etor.hs
@@ -6,7 +6,6 @@ import Control.Monad.Error (MonadError (throwError))
 import Data (LispVal (..))
 import Error (LispError (BadSpecialForm, NotFunction, NumArgs, TypeMismatch), ThrowsError)
 
-
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
 eval val@(Number _) = return val
@@ -30,8 +29,31 @@ primitives =
     ("/", numericBinop div),
     ("mod", numericBinop mod),
     ("quotient", numericBinop quot),
-    ("remainder", numericBinop rem)
+    ("remainder", numericBinop rem),
+    ("=", numBoolBinop (==)),
+    ("<", numBoolBinop (<)),
+    (">", numBoolBinop (>)),
+    ("/=", numBoolBinop (/=)),
+    (">=", numBoolBinop (>=)),
+    ("<=", numBoolBinop (<=)),
+    ("&&", boolBoolBinop (&&)),
+    ("||", boolBoolBinop (||)),
+    ("string=?", strBoolBinop (==)),
+    ("string<?", strBoolBinop (<)),
+    ("string>?", strBoolBinop (>)),
+    ("string<=?", strBoolBinop (<=)),
+    ("string>=?", strBoolBinop (>=))
   ]
+
+boolBinOp :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinOp unpacker op args =
+  if length args /= 2
+    then throwError $ NumArgs 2 args
+    else do
+      --error monad
+      left <- unpacker $ head args
+      right <- unpacker $ args !! 1
+      return $ Bool $ left `op` right
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
@@ -39,9 +61,29 @@ numericBinop op params = Number . foldl1 op <$> mapM unpackNum params
 
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
-unpackNum (String n) = let parsed = reads n in
-                           if null parsed
-                             then throwError $ TypeMismatch "number" $ String n
-                             else return $ fst $ head parsed
+unpackNum (String n) =
+  let parsed = reads n
+   in if null parsed
+        then throwError $ TypeMismatch "number" $ String n
+        else return $ fst $ head parsed
 unpackNum (List [n]) = unpackNum n
-unpackNum notNum     = throwError $ TypeMismatch "number" notNum
+unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+numBoolBinop :: (Integer -> Integer -> Bool) -> [LispVal] -> ThrowsError LispVal
+numBoolBinop = boolBinOp unpackNum
+
+strBoolBinop :: (String -> String -> Bool) -> [LispVal] -> ThrowsError LispVal
+strBoolBinop = boolBinOp unpackStr
+
+boolBoolBinop :: (Bool -> Bool -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBoolBinop = boolBinOp unpackBool
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s) = return $ show s
+unpackStr notString = throwError $ TypeMismatch "string" notString
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
