20e2119c7ee124bfb6a8e6171bd37f8a7df16aea
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-09-26 17:38:17 +0530

    instance Show LispVal

--- app/Data.hs
+++ app/Data.hs
@@ -1,6 +1,8 @@
 -- {-# LANGUAGE StrictData #-}
 module Data where
 
+-- import Etor (showVal)
+
 data LispVal
   = Atom !String
   | List ![LispVal]
@@ -8,3 +10,17 @@ data LispVal
   | Number !Integer
   | String !String
   | Bool !Bool
+
+instance Show LispVal where show = showVal
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number content) = show content
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ "." ++ showVal tail
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
