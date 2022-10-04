b6443f8235c4fa57e2009d6e28c1d4c2c9fcc7cf
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-10-04 15:50:20 +0530

    until_

--- app/Repl.hs
+++ app/Repl.hs
@@ -17,3 +17,7 @@ evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >
 evalAndPrint :: String -> IO ()
 evalAndPrint expr = evalString expr >>= putStrLn
 
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result then return () else action result >> until_ pred prompt action
