d7ac5c57e16089dee43d002833eb1af03fc9a85b
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-10-04 16:23:25 +0530

    isBound

--- app/Vars.hs
+++ app/Vars.hs
@@ -3,8 +3,9 @@
 module Vars where
 
 import Control.Monad.Error (ErrorT (runErrorT), MonadError (throwError))
-import Data ( LispVal )
-import Data.IORef ( IORef, newIORef )
+import Data (LispVal)
+import Data.IORef (IORef, newIORef, readIORef)
+import Data.Maybe (isJust)
 import Error (LispError, ThrowsError, extractValue, trapError)
 
 type Env = [(String, IORef LispVal)]
@@ -18,5 +19,9 @@ liftThrows (Right val) = return val
 -- runIOThrows :: IOThrowsError String -> IO String
 runIOThrows :: Monad f => ErrorT LispError f String -> f String
 runIOThrows action = extractValue <$> runErrorT (trapError action)
+
+-- isBound :: Env -> String -> IO Bool
+isBound :: Eq a => IORef [(a, b)] -> a -> IO Bool
+isBound envRef var = isJust . lookup var <$> readIORef envRef
 
 nullEnv = newIORef []
