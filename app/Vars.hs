3280b3576202f73d2f9039b6f0fd6882673ded48
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-10-04 16:31:26 +0530

    big bug fixes

--- app/Vars.hs
+++ app/Vars.hs
@@ -2,26 +2,29 @@
 
 module Vars where
 
-import Control.Monad.Error (ErrorT (runErrorT), MonadError (throwError))
+import Control.Monad.Error (ErrorT (runErrorT), MonadError (throwError), MonadIO (liftIO))
 import Data (LispVal)
 import Data.IORef (IORef, newIORef, readIORef)
 import Data.Maybe (isJust)
-import Error (LispError, ThrowsError, extractValue, trapError)
+import Error (LispError (..), ThrowsError, extractValue, trapError)
 
-type Env = [(String, IORef LispVal)]
+type Env = IORef [(String, IORef LispVal)]
 
-type IOThrowsError = ErrorT LispVal IO
+type IOThrowsError = ErrorT LispError IO
 
 liftThrows :: MonadError e m => Either e a -> m a
 liftThrows (Left err) = throwError err
 liftThrows (Right val) = return val
 
--- runIOThrows :: IOThrowsError String -> IO String
-runIOThrows :: Monad f => ErrorT LispError f String -> f String
+runIOThrows :: IOThrowsError String -> IO String
 runIOThrows action = extractValue <$> runErrorT (trapError action)
 
--- isBound :: Env -> String -> IO Bool
-isBound :: Eq a => IORef [(a, b)] -> a -> IO Bool
+isBound :: Env -> String -> IO Bool
 isBound envRef var = isJust . lookup var <$> readIORef envRef
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do
+  env <- liftIO $ readIORef envRef
+  maybe (throwError $ UnboundVar "Getting an unbound variable" var) (liftIO . readIORef) (lookup var env)
 
 nullEnv = newIORef []
