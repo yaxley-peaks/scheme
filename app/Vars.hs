90715420b9993a5c29b184fe2dc1fe0d966d945a
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-10-04 16:14:49 +0530

    liftthrows + runiothrows

--- app/Vars.hs
+++ app/Vars.hs
@@ -1,5 +1,22 @@
+{-# OPTIONS_GHC -Wno-deprecations #-}
+
 module Vars where
 
-import Data.IORef
-import Data
+import Control.Monad.Error (ErrorT (runErrorT), MonadError (throwError))
+import Data ( LispVal )
+import Data.IORef ( IORef, newIORef )
+import Error (LispError, ThrowsError, extractValue, trapError)
+
 type Env = [(String, IORef LispVal)]
+
+type IOThrowsError = ErrorT LispVal IO
+
+liftThrows :: MonadError e m => Either e a -> m a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+-- runIOThrows :: IOThrowsError String -> IO String
+runIOThrows :: Monad f => ErrorT LispError f String -> f String
+runIOThrows action = extractValue <$> runErrorT (trapError action)
+
+nullEnv = newIORef []
